2cce0fdd32fbb9018ac669b7cf805644d49cc5af
shmizan <jointdogg@gmail.com>
2021-10-18 20:44:08 +0300

    restore logger
    somehow misplaced during move to gradle

---
+++ src/main/java/com/nbs/hebsubdl/Logger.java
@@ -0,0 +1,66 @@
+package com.nbs.hebsubdl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.logging.*;
+
+public class Logger {
+    public static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("HebSubDL");
+
+    public static void initLogger() {
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("log.log", 1000000, 2,true);
+            logger.addHandler(fileHandler);
+
+            logger.setLevel(Level.FINEST);
+            fileHandler.setFormatter(new SimpleFormatter() {
+                private static final String format
+                        = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+
+                // Override format method
+                @Override
+                public synchronized String format(
+                        LogRecord logRecord)
+                {
+                    return String.format(
+                            format, new Date(logRecord.getMillis()),
+                            logRecord.getLevel().getLocalizedName(),
+                            logRecord.getMessage());
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void logException(Exception e, String action) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String exceptionAsString = sw.toString();
+        logger.severe("caught exception while " + action + ", printing stack trace:");
+        logger.severe(exceptionAsString);
+    }
+
+    public static void logExceptionAndExitWithError(Exception e, String action) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        String exceptionAsString = sw.toString();
+        logger.severe("caught exception while " + action + ", printing stack trace:");
+        logger.severe(exceptionAsString);
+        Logger.logger.info("exiting.");
+        for (Handler h : Logger.logger.getHandlers())
+            h.close();
+        System.exit(-1);
+    }
+
+    public static void logSevereAndExitWithError(String logMsg) {
+        logger.severe(logMsg);
+        Logger.logger.info("exiting.");
+        for (Handler h : Logger.logger.getHandlers())
+            h.close();
+        System.exit(-1);
+    }
+}
